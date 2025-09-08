b318ff9359d7c44d54e8369a4a199ab1f60dce80
juligave2306 <j.gaviriav@uniandes.edu.co>
2025-09-08 22:47:46 +0000

    oráculo

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 const imagenes = [
   "https://www.postposmo.com/wp-content/uploads/2025/08/Diversidad-acuatica-conoce-a-los-animales-que-nadan-en-nuestros-mares-1.jpg.webp",
   "https://www.postposmo.com/wp-content/uploads/2025/08/Delfines-e1755072886737.jpg",
+    "https://www.postposmo.com/wp-content/uploads/2025/08/Caballito-de-mar-scaled-e1755072865180.jpg",
+  "https://www.postposmo.com/wp-content/uploads/2022/05/pulpo.jpg.webp",
 ];
 
 const titulos = [
@@ -13,8 +15,12 @@ const titulos = [
 
 const frases = [
   "Te ves tierna, pero eres peligrosa,la unión es tu poder:confía en tu círculo más cercano.",
-  "Nada con  el ritmo de tu corazón, est.",
+  "Nada con  el ritmo de tu corazón, estás llena de alegría, comunicación y armonía.",
+  "Aférrate  a tu calma y estabilidad eso mantiene en armonìa tu propio ritmo vital.",
+  "Transforma tu entorno con creatividad y fluidez; tu inteligencia te guía a través de múltiples caminos.",
 ];
+
+
 
 // Trae los elementos del HTML que tienen ese id
 const imagen = document.getElementById("foto");
@@ -22,14 +28,17 @@ const titulo = document.getElementById("titulo1");
 const prediccion = document.getElementById("prediccion1");
 const boton = document.getElementById("boton");
 
+
 // Función para generar un número aleatorio entre un valor mínimo y un máximo
 function numeroRandom(min, max) {
   return Math.ceil(Math.random() * (max - min) + min - 1);
 }
 
-function que animal marino eres() {
+function queAnimalMarinoEres() {
   // Para elegir la misma posición en todas las listas, generar un solo número aleatorio
-  const indice = numeroRandom(0, frases.lenght); // 0, 1, 2, 3, 4  
+  const indice = numeroRandom(0, frases.length); // 0, 1, 2, 3, 4 
+  
+  console.log(indice)
 
   // Reemplaza el texto del elemento 'titulo' por un título de la lista de títulos elegido aleatoriamente
   titulo.innerText = titulos[indice]; // Entre los [] se pasa el número aleatorio generado arriba
@@ -43,7 +52,7 @@ function que animal marino eres() {
 
 // Ejecuta la función generarPoema() cada vez que hago click en el botón
 boton.addEventListener("click", function () {
-  generarPrediccion();
+  queAnimalMarinoEres();
 });
 
-generarPrediccion();
+queAnimalMarinoEres();
